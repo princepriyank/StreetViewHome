d252f42f440b63248132e16a3db785b36381ac68
princepriyank <mishrapriyank17@gmail.com>
2021-01-03 23:45:51 +0530

    initial commits wit working model

---
+++ src/details.js
@@ -0,0 +1,32 @@
+export const Detail=[
+    {
+       id: 0,
+       image: "new.jpeg",
+       pitch1: 184.41,
+       pitch2: 165,
+       yaw1: 154.76,
+       yaw2: 210,
+       id1: 1,
+       id2: 2,
+    },
+    {
+        id: 1,
+        image: "old.jpeg",
+        pitch1: 190.41,
+        pitch2: 155,
+        yaw1: 200.76,
+        yaw2: 140,
+        id1: 0,
+        id2: 2,
+     },
+     {
+        id: 2,
+        image: "old2.jpeg",
+        pitch1: 190.41,
+        pitch2: 165,
+        yaw1: 180.76,
+        yaw2: 210,
+        id1: 0,
+        id2: 1,
+     }
+];
